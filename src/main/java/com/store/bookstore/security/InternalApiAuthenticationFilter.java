2bcff59d78b79963c236e71c6cfddea04087e436
Diouani <diouani.youssef@gmail.com>
2022-05-01 14:32:18 +0000

    Mise en place de l'internal filter

--- src/main/java/com/store/bookstore/security/InternalApiAuthenticationFilter.java
+++ src/main/java/com/store/bookstore/security/InternalApiAuthenticationFilter.java
@@ -1,5 +1,9 @@
 package com.store.bookstore.security;
 
+import com.store.bookstore.util.SecurityUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -7,8 +11,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 
+@Slf4j
 public class InternalApiAuthenticationFilter extends OncePerRequestFilter {
 
 
@@ -21,5 +27,22 @@ public class InternalApiAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+
+        try {
+            String requestkey = SecurityUtils.extractAuthTokenFromRequests(request);
+            if (requestkey == null || requestkey.equals(accessKey)) {
+                log.warn("internal key endpoint requested  without/ with wrong  key uri:{}"
+                        , request.getRequestURI());
+                throw new RuntimeException("UNAUTORIZED");
+            }
+
+            UserPrincipal user = UserPrincipal.createSuperUser();
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (Exception ex) {
+            log.error("Could not set user authentication in security context", ex);
+        }
+        filterChain.doFilter(request, response);
     }
 }
--- src/main/java/com/store/bookstore/security/UserPrincipal.java
+++ src/main/java/com/store/bookstore/security/UserPrincipal.java
@@ -1,6 +1,8 @@
 package com.store.bookstore.security;
 
+import com.store.bookstore.model.Role;
 import com.store.bookstore.model.User;
+import com.store.bookstore.util.SecurityUtils;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,6 +25,15 @@ public class UserPrincipal implements UserDetails {
     transient private String password;
     transient private User user;
     private Set<GrantedAuthority> authorities;
+
+    public static UserPrincipal createSuperUser(){
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAuthority(Role.SYSTEM_MANAGER.name()));
+        return UserPrincipal.builder()
+                .id(-1L)
+                .username("system-administrator")
+                .authorities(authorities)
+                .build();
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
